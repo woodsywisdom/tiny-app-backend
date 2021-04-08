2b674483bba98999323429b71b8287daec61070c
Will <wmaccarty@gmail.com>
2021-04-08 17:50:08 -0400

    implemented post, get, patch, and delete routes

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const app = express();
 const mongoose = require('mongoose');
 
 const db = require('./config/keys').mongoURI;
+const questions = require('./routes/api/questions');
+
+app.use(express.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("mongoose has found a snek"))
   .catch( err => console.log(err));
-
-
 app.get('/', (req, res) => res.send('This is a thing'));
+app.use('/api/questions', questions);
+
 
 const port = process.env.PORT || 5000;
 
