f8baf5923e1032079178160778221e5bc6d25c0a
Will <wmaccarty@gmail.com>
2021-04-08 14:46:27 -0400

    built Question schema using mongoose

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongoose has found a snek"))
+  .catch( err => console.log(err));
+
+
 app.get('/', (req, res) => res.send('This is a thing'));
 
 const port = process.env.PORT || 5000;
